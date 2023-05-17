253fc3eee0df1187c168b744886a8d11fe3ec067
Guraeng <guraeng2@gmail.com>
2023-05-17 17:39:40 +0900

    Feat : 로그인 버튼 추가

--- frontend/src/components/CardTest.vue
+++ frontend/src/components/CardTest.vue
@@ -1,10 +1,28 @@
 <template>
-  <v-card title="Card title" subtitle="Subtitle" text="블라블라블라">
+  <v-card title="SSO 구현해보기" subtitle="OIDC & OAuth2.0"
+    text="Access Token과 ID Token을 얻어서 뿌릴 수 있어야 한다. 회원가입 및 로그인, 로그아웃과 같은 동작이 가능해야한다.">
     <v-card-actions>
-      <v-btn>Click me</v-btn>
+      <v-btn @click="login" prepend-icon="mdi-login">LOGIN</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import axios from "axios";
+
+export default {
+  methods: {
+    login() {
+      alert("LOGIN BUTTON");
+      axios.get('http://localhost:3000/auth/login')
+        .then(response => {
+          // Express 백엔드로 리디렉션된 후에는 인증 서비스에서 로그인을 수행합니다.
+          alert(response);
+        })
+        .catch(error => {
+          alert(error);
+        });
+    }
+  }
+};
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,6 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
-
 createApp(App)
   .use(vuetify)
   .mount('#app')
