2ac337516223e6c096784dd7b727d9af1999d965
Guraeng <guraeng2@gmail.com>
2023-05-17 16:53:40 +0900

    Feat : Apply base vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,49 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="John Leider" nav>
+          <template v-slot:append>
+            <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-text-box-multiple-outline" title="Documents" value="documents"></v-list-item>
+          <v-list-item prepend-icon="mdi-calendar-month" title="Calendar" value="calendar"></v-list-item>
+          <v-list-item prepend-icon="mdi-folder-account" title="Projects" value="projects"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar title="Home"></v-app-bar>
+      <!-- <v-app-bar color="grey-lighten-2" flat location="bottom"></v-app-bar> -->
+      <v-main>
+        <v-container>
+          <CardTest></CardTest>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CardTest from './components/CardTest.vue'
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    CardTest
   },
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: 'Home', icon: 'mdi-home' },
+        { title: 'Documents', icon: 'mdi-text-box-multiple-outline' },
+        { title: 'Calendar', icon: 'mdi-calendar-month' },
+        { title: 'Projects', icon: 'mdi-folder-account' },
+      ],
+      rail: true,
+    }
+  },
 }
 </script>
---
+++ frontend/src/components/CardTest.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-card title="Card title" subtitle="Subtitle" text="블라블라블라">
+    <v-card-actions>
+      <v-btn>Click me</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+</script>
